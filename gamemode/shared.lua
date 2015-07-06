98147781000d0349d024d16bbd51c12c8b881490
Zelpa <zelpahd@gmail.com>
2015-07-06 19:10:02 +1000

    teams? i think

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -10,3 +10,10 @@ function GM:Initialize()
 	
 end
 
+function GM:CreateTeams()
+	team.SetUp(1, "Combine", Color( 142, 68, 173 ), true)
+	team.SetUp(2, "Rebels", Color( 211, 84, 0 ), true)
+
+	team.SetSpawnPoint( 1, "info_player_start" )
+	team.SetSpawnPoint( 2, "info_player_start" )
+end
